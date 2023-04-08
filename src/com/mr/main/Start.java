ba36abf887427da05831dec5d583ee5be2b21904
binka <12753297+binka@user.noreply.gitee.com>
2023-04-08 12:50:55 +0000

    update src/com/mr/main/Start.java.
    Signed-off-by: binka <12753297+binka@user.noreply.gitee.com>

--- src/com/mr/main/Start.java
+++ src/com/mr/main/Start.java
@@ -3,15 +3,15 @@ package com.mr.main;
 import com.mr.view.MainFrame;
 
 /**
- * 入口类
  * 
- * @author 格伦的奇妙冒险
+ * 
+ * @author 鏃犵梾
  *
  */
 public class Start {
 
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame();// 创建主窗体
-        frame.setVisible(true);// 显示主窗体
+        MainFrame frame = new MainFrame();// 
+        frame.setVisible(true);// 示
     }
 }
