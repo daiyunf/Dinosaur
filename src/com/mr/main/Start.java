2d7b5347ec49d0b6644913612fa66b528c5fa2cb
daiyunfeng <484836894@qq.com>
2023-04-09 19:08:49 +0800

    dyf

---
+++ src/com/mr/main/Start.java
@@ -0,0 +1,17 @@
+package com.mr.main;
+
+import com.mr.view.MainFrame;
+
+/**
+ * 入口类
+ * 
+ * @author 格伦的奇妙冒险
+ *
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();// 创建主窗体
+        frame.setVisible(true);// 显示主窗体
+    }
+}
